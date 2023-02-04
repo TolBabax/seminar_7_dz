9793670ce5f182917ed8d923ed1c68470f314c82
TolBabax <kuz.cerg@gmail.com>
2023-02-04 21:22:48 +0400

    Поиск требуемого числе в массиве

---
+++ task2/Program.cs
@@ -0,0 +1,70 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] FillMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matr = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.Next(0, 10);
+        }
+    }
+
+    return matr;
+}
+
+bool NumberCheck(int num, int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (num == array[i,j])
+                return true;
+        }
+    }
+    return false;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = FillMatrix(m, n);
+System.Console.WriteLine();
+PrintMatrix(matrix);
+System.Console.WriteLine();
+
+if (NumberCheck(A, matrix))
+{
+    System.Console.WriteLine($"Число {A} есть в данном массиве");
+}
+else
+{
+    System.Console.WriteLine($"Число {A} нет в данном массиве");
+}
