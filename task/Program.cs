8f6d69305b4639c352b594168d6f16e0e40e8d6b
TolBabax <kuz.cerg@gmail.com>
2023-02-04 19:26:28 +0400

    двумерный массив размером m×n, заполненный случайными вещественными числами.

---
+++ task/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] FillMatrix(int rows, int cols)
+{
+    double[,] matrix = new double[rows, cols];
+    var rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = Math.Round(rand.Next(0, 10) + rand.NextDouble(), 2);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+double[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+
